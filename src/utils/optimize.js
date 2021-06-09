645337e58a55c5c2fa74c6e50fe26aad5b8e01af
Stan van Heumen <stanvanheumen@gmail.com>
2021-06-09 13:53:33 +0200

    updates.

--- src/utils/optimize.js
+++ src/utils/optimize.js
@@ -1,16 +1,13 @@
 const {readFile} = require('./files');
-const SVGO = require('svgo');
+const {optimize} = require('svgo');
 
 module.exports.execute = async icons => {
-    // Create the SVGO object.
-    const svgo = getSvgoObject();
-
     for (let icon of icons) {
         // Read the string data of the file.
         const data = await readFile(icon.path);
 
         // Optimize the SVG data.
-        const result = await svgo.optimize(data, {path: icon.path});
+        const result = await optimize(data, {path: icon.path});
 
         // Set the icon data.
         icon.data = result.data;
@@ -18,43 +15,3 @@ module.exports.execute = async icons => {
 
     return icons;
 };
-
-function getSvgoObject() {
-    return new SVGO({
-        plugins: [
-            {cleanupAttrs: true},
-            {removeDoctype: true},
-            {removeXMLProcInst: true},
-            {removeComments: true},
-            {removeMetadata: true},
-            {removeTitle: true},
-            {removeDesc: true},
-            {removeUselessDefs: true},
-            {removeEditorsNSData: true},
-            {removeEmptyAttrs: true},
-            {removeHiddenElems: true},
-            {removeEmptyText: true},
-            {removeEmptyContainers: true},
-            {removeViewBox: false},
-            {cleanupEnableBackground: true},
-            {convertStyleToAttrs: true},
-            {convertColors: true},
-            {convertPathData: true},
-            {convertTransform: true},
-            {removeUnknownsAndDefaults: true},
-            {removeNonInheritableGroupAttrs: true},
-            {removeUselessStrokeAndFill: true},
-            {removeUnusedNS: true},
-            {cleanupIDs: true},
-            {cleanupNumericValues: true},
-            {moveElemsAttrsToGroup: true},
-            {moveGroupAttrsToElems: true},
-            {collapseGroups: true},
-            {removeRasterImages: false},
-            {mergePaths: true},
-            {convertShapeToPath: true},
-            {removeDimensions: true},
-            {minifyStyles: true}
-        ]
-    });
-}
